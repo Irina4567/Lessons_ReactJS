2bf4f0b590e8e4ab61a5c708b875b180daffe0e6
Irina <irina.pavliuchenkova@yandex.ru>
2021-09-07 21:43:48 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello world !!!
+          fksjdfkjsdvkjdnsv
         </p>
       </header>
     </div>
